05d5d17c0b268828fb0b36c8506041fe8f13ea9c
AyushMaurya-tech <ayush_2401ct23@iitp.ac.in>
2025-11-06 15:08:09 +0530

    Initial commit

--- mobile_sam_onnx/export_sam_mobile_encoder.py
+++ mobile_sam_onnx/export_sam_mobile_encoder.py
@@ -1,15 +1,13 @@
 import torch
 from mobile_sam import sam_model_registry
 
-checkpoint = "/home/logan78/Desktop/adobe/models/mobile_sam.pt"
-
-# Load MobileSAM
+checkpoint = "../mobile_sam_onnx/mobile_sam.pt"
 model = sam_model_registry["vit_t"](checkpoint=checkpoint)
 model.eval()
 
 encoder = model.image_encoder
 decoder = model.mask_decoder
-image_pe = model.prompt_encoder.get_dense_pe()  # ✅ positional embedding
+image_pe = model.prompt_encoder.get_dense_pe() 
 
 # ------------------ Export Encoder ------------------
 dummy_image = torch.randn(1, 3, 1024, 1024)
@@ -23,9 +21,7 @@ torch.onnx.export(
     opset_version=17,
     do_constant_folding=True,
 )
-print("✅ Encoder saved as mobile_sam_encoder.onnx")
-
-# ------------------ Wrap Decoder ------------------
+print("Encoder saved as mobile_sam_encoder.onnx")
 class DecoderWrapper(torch.nn.Module):
     def __init__(self, decoder, image_pe):
         super().__init__()
@@ -33,10 +29,10 @@ class DecoderWrapper(torch.nn.Module):
         self.image_pe = image_pe
 
     def forward(self, image_embedding, sparse_prompt_embeddings, dense_prompt_embeddings):
-        # multimask_output=False (single best mask)
+
         masks, iou_scores = self.decoder(
             image_embeddings=image_embedding,
-            image_pe=self.image_pe,                      # ✅ REQUIRED
+            image_pe=self.image_pe,                      
             sparse_prompt_embeddings=sparse_prompt_embeddings,
             dense_prompt_embeddings=dense_prompt_embeddings,
             multimask_output=False,
@@ -45,12 +41,10 @@ class DecoderWrapper(torch.nn.Module):
 
 wrapper = DecoderWrapper(decoder, image_pe)
 
-# Dummy inputs for tracing
 image_embedding = encoder(dummy_image)
 sparse = torch.zeros(1, 2, decoder.transformer_dim)
 dense = torch.zeros(1, decoder.transformer_dim, 64, 64)
 
-# ------------------ Export Decoder ------------------
 torch.onnx.export(
     wrapper,
     (image_embedding, sparse, dense),
@@ -61,4 +55,4 @@ torch.onnx.export(
     do_constant_folding=True,
 )
 
-print("🎉 Decoder saved as mobile_sam_decoder.onnx")
+print("Decoder saved as mobile_sam_decoder.onnx")
